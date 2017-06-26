5ae7e61750e81df0c471fc56a21fad2b7507be21
Somesh Singh <somesh.singh1992@gmail.com>
2017-06-26 08:35:28 +0200

    reorganizing

--- driverprog.cu
+++ driverprog.cu
@@ -1,84 +1,48 @@
-/*Question: how do we allocate different vectors in the arena and how to distinguish between different vectors?*/
-
-/*Compare the code with malloc() in GPU kernel*/
-
-
 #include <stdio.h>
 #include <iostream>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <vector>
 #include <array>
-#include "GPUArena.h"
+#include "salloc.h"
 #include "timer.h"
 
-__global__ void kernel(GPUArena<2,32,int> ga1)
+
+#define CHUNK_SZ 32
+#define NUM_LAYERS 8
+#define CAPACITY 2048 // 2^11
+
+
+__global__ void kernel(GPUArena<NUM_LAYERS,CHUNK_SZ,int> ga1)
 {
    
-   printf("number of elements in layer %d = %d\n",threadIdx.x,ga1.get_num_elements_per_layer(threadIdx.x)); // this works
    int x[5]= {10,11,12,13,14}; // each thread pushs back an arrary and not a scalar
-   int u[3]= {201,202,203}; // each thread pushs back an arrary and not a scalar
-   int v[3]= {310,31,123}; // each thread pushs back an arrary and not a scalar
-   printf("hello1\n");
-   ga1.push_back(0,1,x); 
-   printf("hello2\n");
-   ga1.push_back(0,1,u); 
-   printf("hello3\n");
-   ga1.push_back(0,1,v); 
-   printf("hello4\n");
-//   ga1.push_back(0,2,v); 
- //  printf("next free chunk = %d \n",*(ga1.nextFreeChunk_d));
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
- //  ga1.push_back(0,2,v); 
-//   printf("x[%d] = %d\n",1,x[1]);
-   GPUArenaIterator<32, int> iter = ga1.iterator(0,1); // accessing and traversing an arena
-//   printf("iter.cursorInChunk = %d", iter.cursorInChunk);
-   int *y = iter.get_next();
-   printf("y[%d] = %d \n",0,y[0]);
-//   printf("iter.cursorInChunk = %d", iter.cursorInChunk);
-   int* z = iter.get_next();
-   printf("z[%d] = %d \n",0,z[0]);
-//   printf("iter.cursorInChunk = %d", iter.cursorInChunk);
-   int* k = iter.get_next();
-   printf("k[%d] = %d \n",0,k[0]);
+   int _eleId = threadIdx.x; 
+   int _layer = blockIdx.x;
+   ga1.push_back(_layer,_eleId,x); 
+//   GPUArenaIterator<32, int> iter = ga1.iterator(_layer,_eleId); // accessing and traversing an arena
+//   int *y = iter.get_next(); // get the first vector in a chunk 
+//   printf("y[%d] = %d \n",0,y[0]); // accessing an element in the vector in the chunk
+//   int* z = iter.get_next();
+//   printf("z[%d] = %d \n",0,z[0]);
+//   int* k = iter.get_next();
+//   printf("k[%d] = %d \n",0,k[0]);
 }
-
-//__global__ void kernel1(GPUArena<2,32,int> ga2)
-//{
-//   int v[2]= {310,31}; // each thread pushs back an arrary and not a scalar
-//   ga2.push_back(0,2,v); 
-//  
-//   GPUArenaIterator<32, int> iter = ga2.iterator(0,2); // accessing and traversing an arena
-////   printf("iter.cursorInChunk = %d", iter.cursorInChunk);
-//   int *y = iter.get_next();
-//   printf("y[%d] = %d \n",0,y[0]);
-////   printf("iter.cursorInChunk = %d", iter.cursorInChunk);
-// // int* z = iter.get_next();
-//  // printf("z[%d] = %d \n",0,z[0]);
-//  // printf("iter.cursorInChunk = %d", iter.cursorInChunk);
-//  // int* k = iter.get_next();
-//  // printf("k[%d] = %d \n",0,k[0]);
-//}
-
 
 int main(void)
 {
-  std::array<int,2> arr= {4, 4}; // number of elements in each layer
+
+  GpuTimer timer;
+  std::array<int,NUM_LAYERS> arr= {256,256,256,256,256,256,256,256}; // number of elements in each layer
  // note: capacity = sum of no. of elements in each layer
-  GPUArena<2,32,int> ga1(2,arr); // creating an arena with proper initial paramerters.
-  //GPUChunk<32,int> gc;
-  
-  kernel<<<1,2>>>(ga1); // this works.
+  GPUArena<NUM_LAYERS,CHUNK_SZ,int> ga1(CAPACITY,arr); // creating an arena with proper initial paramerters.
+  unsigned block_sz = 32;
+  unsigned grid_sz =  NUM_LAYERS;
+  timer.Start();
+  kernel<<<grid_sz,block_sz>>>(ga1); // invoking the kernel where push_back() is performed..
+  timer.Stop();
   cudaDeviceSynchronize();
-  //kernel1<<<1,2>>>(ga1);
- // cudaDeviceSynchronize();
+  printf("Kernel code ran in: %f msecs.\n", timer.Elapsed());
   ga1.~GPUArena();
   return 0;
 
