63378bdee68469191fb2cac989551ac5a470ee32
Somesh Singh <somesh.singh1992@gmail.com>
2017-08-09 08:21:50 +0200

    adding todos

---
+++ salloc_phase3.h
@@ -0,0 +1,11 @@
+
+/* TODOs:
+ * 1. implement push_back()
+ * 2. implement pop_back()
+ * 3. implement the API: arena.createVector([size]) -- initial size of the vector is an optional parameter.
+ *    Support for multiple vectors on the arena.
+ * 4. implement getIndex(): maps the index of a vector to the corrensponding index on the arena. 
+ * */
+
+
+
