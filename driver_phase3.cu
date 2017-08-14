c814b970f2ffce5c230fe665818316a39b7a044f
Somesh Singh <somesh.singh1992@gmail.com>
2017-08-14 08:25:49 +0200

    more edits

--- driver_phase3.cu
+++ driver_phase3.cu
@@ -1,1 +1,19 @@
 // This is the driver program for SALLOC
+
+#include <stdio.h>
+#include <cuda.h>
+#include "salloc_phase3.h"
+
+#define CHUNK_SZ 32 // size of a chunk
+#define CAP 16 // number of chunks in the chunk
+
+typedef int T1; // 
+
+int main(int argc, char** argv)
+{
+  GPUArena<CHUNK_SZ, T1> arena(CAP);
+  
+/* This is the desired API for create vector  */
+// GPUChunk<CHUNK_SZ, T1> * v1 = arena.createVector(); // returns the address of the next fully free chunk in arena(on GPU) and stores it on v1 on the CPU
+  return 0;
+}
--- salloc_phase3.h
+++ salloc_phase3.h
@@ -7,5 +7,68 @@
  * 4. implement getIndex(): maps the index of a vector to the corrensponding index on the arena. 
  * */
 
+/**
+ * GOAL-1: implement support for multiple vectors on the arena
+ */
 
+#ifndef SALLOC_H
+#define SALLOC_H
+
+#include <cuda_runtime.h>
+#include <assert.h>
+
+#define checkCudaError(val) __checkCudaError__ ( (val), #val, __FILE__, __LINE__ )
+
+template <typename T>
+inline void __checkCudaError__(T code, const char *func, const char *file, int line)
+{
+  if (code) {
+    fprintf(stderr, "CUDA error at %s:%d: %s (code=%d)\n",
+        file, line, cudaGetErrorString(code), (unsigned int)code);
+    cudaDeviceReset();
+    exit(EXIT_FAILURE);
+  }
+}
+
+#define checkLastCudaError() checkCudaError( (cudaGetLastError()) )
+
+// Creating the arena
+ 
+template <int CHUNK_SIZE, typename T>
+class GPUChunk {
+  public:
+    T values[CHUNK_SIZE];
+    int nextFreeValue;
+    // required for push_back()
+    GPUChunk<CHUNK_SIZE, T> *next; // pointer to the next chunk
+    // required for pop_back()
+    GPUChunk<CHUNK_SIZE, T> *prev; // pointer to the previous chunk
+};
+
+
+template <int CHUNK_SIZE, typename T>
+class GPUArena {
+private:
+    // chunks points to the arena
+    GPUChunk<CHUNK_SIZE, T> *chunks;
+    //total number of chunks
+    int capacity;
+   
+public: 
+    int *nextFreeChunk_d; // points to the free chunk
+    GPUArena(int _capacity)
+      : capacity(_capacity)
+    {
+      //allocate the main arena storage and set everything to 0 (important
+      //because the counters in each chunk must be )
+      checkCudaError(cudaMalloc(&chunks, sizeof(GPUChunk<CHUNK_SIZE, T>) * capacity));
+      checkCudaError(cudaMemset(chunks, 0, sizeof(GPUChunk<CHUNK_SIZE, T>) * capacity));
+
+      checkCudaError(cudaMalloc(&nextFreeChunk_d, sizeof(int)));
+//      checkCudaError(cudaMemset(nextFreeChunk_d, 0, sizeof(int)));
+
+  }
+};
+
+#endif
 
