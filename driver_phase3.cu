55e62e38353b7a52fe5ac6aeca5769e74efe4a20
Somesh Singh <somesh.singh1992@gmail.com>
2017-08-15 17:27:46 +0200

    minor edits

--- driver_phase3.cu
+++ driver_phase3.cu
@@ -19,7 +19,9 @@ __global__ void kernel(GPUArena<CHUNK_SZ,T1> a )
 __global__ void kernel1(GPUArena<CHUNK_SZ,T1> a, GPUChunk<CHUNK_SZ,T1>* v)
 {
   printf("%p\n",v); 
-  //v->values[0] = 5; 
+  v->values[0] = 5; 
+  v->values[1] = 2; 
+  printf("%d\n",v->values[0]); 
   //printf("%d\n",*(v + sizeof(T1))); 
 }
 
@@ -31,7 +33,7 @@ int main(int argc, char** argv)
 // GPUChunk<CHUNK_SZ, T1> * v1 = arena.createVector(); // returns the address of the next fully free chunk in arena(on GPU) and stores it on v1 on the CPU
   kernel<<<1,8>>>(arena); 
   cudaDeviceSynchronize();
-  GPUChunk<CHUNK_SZ,T1> * v1 = arena.createVector();
+  GPUChunk<CHUNK_SZ,T1> * v1 = arena.createVector(); // 'v1' points to a chunk and not to the array inside the chunk.
   kernel1<<<1,1>>>(arena, v1);
   cudaDeviceSynchronize();
   return 0;
