af3ce9e398a8090c2298c37e324af68192b42f59
Somesh Singh <somesh.singh1992@gmail.com>
2017-08-26 16:33:12 +0530

    Update driver.cu

--- driver.cu
+++ driver.cu
@@ -4,17 +4,11 @@
 #include <cuda.h>
 #include "salloc.h"
 
-#define CHUNK_SZ 50 // size of a chunk
-#define CAP 1600 // number of chunks in the chunk
+#define CHUNK_SZ 16 // size of a chunk
+#define CAP 128 // number of chunks in the arena
 
-typedef char T1; // 
+typedef int T1;  
 
-
-__global__ void kernel(GPUArena<CHUNK_SZ,T1> a )
-
-{
- a.get_new_chunk();
-}
 
 __global__ void kernel1(GPUArena<CHUNK_SZ,T1> a, T1* v1, T1* v2, T1* v3 )
 {
@@ -24,61 +18,32 @@ __global__ void kernel1(GPUArena<CHUNK_SZ,T1> a, T1* v1, T1* v2, T1* v3 )
 
 }
 
-__global__ void kernel5(GPUArena<CHUNK_SZ,T1> a, T1* v2)
-{
-  unsigned tid = threadIdx.x;
-   a.push_back(v2,tid); printf("push_back in v1\n");
-
-}
-
-
-__global__ void kernel6(GPUArena<CHUNK_SZ,T1> a, T1* v3)
-{
-  unsigned tid = threadIdx.x;
-   a.push_back(v3,tid); printf("push_back in v1\n");
-
-}
-
 __global__ void kernel2(GPUArena<CHUNK_SZ,T1> a, T1* v1, T1* v3)
 {
   unsigned tid = threadIdx.x;
-  printf("pop value from v1 = %d\n",a.pop_back(v1)); //printf("pop_back from v1\n");
+  printf("pop value from v1 = %d\n",a.pop_back(v1));
    a.push_back(v3,tid); printf("push_back in v3\n");
-  printf("pop value from v3 = %d\n",a.pop_back(v3)); //printf("pop_back from v1\n");
+  printf("pop value from v3 = %d\n",a.pop_back(v3));
 }
 
 
-__global__ void kernel3(GPUArena<CHUNK_SZ,T1> a, T1* v1, T1* v2)
-{
-  unsigned tid = threadIdx.x;
-  a.push_back(v2,tid);
-}
-
-__global__ void kernel4(GPUArena<CHUNK_SZ,T1> a, T1* v1)
+__global__ void kernel3(GPUArena<CHUNK_SZ,T1> a, T1* v1)
 {
   unsigned tid = threadIdx.x;
   printf("global index of v1[%d] = %d\n",tid,a.getIndex(v1,tid));
 }
 
 
-
-
-
 int main(int argc, char** argv)
 {
-  GPUArena<CHUNK_SZ, T1> arena(CAP);
+  GPUArena<CHUNK_SZ, T1> arena(CAP); // creating an arena
   
   T1 * v1 = arena.createVector(); 
   T1 * v2 = arena.createVector(); 
-  T1 * v3 = arena.createVector(); // we can have a parameter 'size' which can be set to CHUNK_SZ by default.
+  T1 * v3 = arena.createVector(); // we can have a parameter 'size' which is set to CHUNK_SZ by default.
   kernel1<<<1,23>>>(arena, v1, v2, v3);
-  //kernel5<<<1,20>>>(arena, v2);
-  //kernel6<<<1,20>>>(arena, v3);
   kernel2<<<1,2>>>(arena, v1,v3);
-//  kernel1<<<1,5>>>(arena, v1,v2,v3);
-//  kernel2<<<1,10>>>(arena, v1,v2,v3);
-//  kernel3<<<1,25>>>(arena, v1);
-  kernel4<<<1,30>>>(arena, v1);
+  kernel3<<<1,30>>>(arena, v1);
   cudaDeviceSynchronize();
   return 0;
 }
